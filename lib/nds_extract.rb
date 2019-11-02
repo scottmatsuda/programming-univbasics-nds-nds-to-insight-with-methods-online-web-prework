3522c710b3d422a6b5c16193a839e5196d55e4ed
Scott Matsuda <smatsuda2014@gmail.com>
2019-11-02 08:12:15 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -11,7 +11,7 @@ def directors_totals(nds)
   
   row_index = 0
   while row_index < nds.length do
-    result[nds][row_index][:name] = gross_for_director()
+    result[nds][row_index][:name] = gross_for_director(nds, row_index)
     row_index += 1
   end
   
